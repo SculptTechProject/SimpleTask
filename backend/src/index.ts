dd82b0d6838478d66f5141b2df970156e32188d9
SculptTechProject <150788324+SculptTechProject@users.noreply.github.com>
2025-02-15 01:35:56 +0100

    cors and prisma!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,9 +9,16 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "*",
+  origin: ["https://simple-task-ten.vercel.app"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+  ],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
